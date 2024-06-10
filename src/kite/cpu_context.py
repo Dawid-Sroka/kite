7ec2d342bb236d4a18495138ab577bba15df5db6
Dawid-Sroka <94313700+Dawid-Sroka@users.noreply.github.com>
2024-06-10 11:10:34 +0200

    Embed page table in vmareas, use abstract interface from pyrisc

--- src/kite/cpu_context.py
+++ src/kite/cpu_context.py
@@ -1,7 +1,7 @@
 from pyrisc.sim.components import Register, RegisterFile, Memory
 from kite.consts import *
 
-from pyrisc.sim.components import PageTable
+from pyrisc.sim.components import TranslatesAddresses, PageTableEntry, VPO_LENTGH, VPN_MASK
 
 #--------------------------------------------------------------------------
 #   Configurations
@@ -17,32 +17,79 @@ DMEM_START  = WORD(0x80010000)      # DMEM: 0x80010000 - 0x8001ffff (64KB)
 DMEM_SIZE   = WORD(64 * 1024)
 #--------------------------------------------------------------------------
 
-class VMAreas:
+class VMAreaStruct:
+    def __init__(self, area_start, area_size, area_prot, area_flags):
+        self.start_vpn = area_start >> VPO_LENTGH
+        self.page_cnt = area_size >> VPO_LENTGH
+        # dictionary of type {vpn : pte}
+        self.cached_pages = {}
+        self.vm_prot = area_prot
+        self.vm_flags = area_flags
+
+    def get_page(self, vpn) -> PageTableEntry:
+        if vpn in self.cached_pages.keys():
+            return self.cached_pages[vpn]
+        else:
+            return None
+
+
+class VMAreas(TranslatesAddresses):
     def __init__(self):
         self.vm_areas_list = []
 
-class VMAreaStruct:
-    def __init__(self, vm_start, vm_end, vm_prot, vm_flags):
-        self.vm_start = vm_start
-        self.vm_end = vm_end
-        self.vm_prot = vm_prot
-        self.vm_flags = vm_flags
+    def translate(self,vpn) -> PageTableEntry | None:
+        area = self.get_area_by_vpn(vpn)
+        pte = area.get_page(vpn)
+        return pte
 
-    def does_contain_address(self, virt_addr):
-        if self.vm_start <= virt_addr and virt_addr < self.vm_end:
-            return True
+    def copy_into_vm(self, va, data):
+        vpn = va >> VPO_LENTGH
+        VPO_MASK = 2**VPO_LENTGH - 1
+        vpo = (va & VPO_MASK) // WORD_SIZE
+        # pte = self.page_table.translate(vpn)
+
+        area = self.get_area_by_vpn(vpn)
+        if area != None:
+            if vpn not in area.cached_pages.keys():
+                area.cached_pages[vpn] = PageTableEntry(vpn)
+                area.cached_pages[vpn].physical_page[vpo] = data
+            else:
+                area.cached_pages[vpn].physical_page[vpo] = data
         else:
-            return False
+            raise NotImplementedError
+
+    def get_area_by_vpn(self, vpn) -> VMAreaStruct:
+        for area in self.vm_areas_list:
+            if area.start_vpn <= vpn and vpn < area.start_vpn + area.page_cnt:
+                return area
+        return None
+
+    def get_area_by_va(self, va) -> VMAreaStruct:
+        vpn = va >> VPO_LENTGH
+        for area in self.vm_areas_list:
+            if area.start_vpn <= vpn and vpn < area.start_vpn + area.page_cnt:
+                return area
+        return None
+
+    def add_page_containing_addr(self, addr):
+        new_page_addr = addr & VPN_MASK
+        vpn = addr >> VPO_LENTGH
+        area = self.get_area_by_va(addr)
+        if area != None:
+            if vpn not in area.cached_pages.keys():
+                area.cached_pages[vpn] = PageTableEntry(vpn)
+        else:
+            raise NotImplementedError
 
 class CPUContext:
-    def __init__(self, pc: Register, regs: RegisterFile, pt: PageTable):
+    def __init__(self, pc: Register, regs: RegisterFile, vm_areas: VMAreas):
         self.pc = pc
         self.regs = regs
-        self.page_table = pt
+        self.vm = vm_areas
 
     @classmethod
     def create(cls):
         pc = Register()
         regs = RegisterFile()
-        page_table = PageTable()
-        return cls(pc, regs, page_table)
+        vm_areas = VMAreas()
+        return cls(pc, regs, vm_areas)
