02387e36da20e8fc08fad4363824d46a6724ce3c
Dawid-Sroka <94313700+Dawid-Sroka@users.noreply.github.com>
2024-07-03 19:05:00 +0200

    Add blocked_queue and moving from/to it in Scheduler

--- src/kite/scheduler.py
+++ src/kite/scheduler.py
@@ -5,6 +5,7 @@ from kite.process import Process
 class Scheduler:
     def __init__(self):
         self.ready_queue = []
+        self.blocked_queue = []
 
     def enqueue_thread(self, thread):
         self.ready_queue.append(thread)
@@ -16,6 +17,14 @@ class Scheduler:
         else:
             thread = self.ready_queue[0]
             return thread
+
+    def move_to_blocked(self, thread):
+        blocked_thread = self.ready_queue.pop(0)
+        self.blocked_queue.append(blocked_thread)
+
+    def move_to_ready(self, thread):
+        ready_thread = self.blocked_queue.pop(0)
+        self.ready_queue.append(ready_thread)
 
     def remove_thread(self) -> None:
         self.ready_queue.pop(0)
