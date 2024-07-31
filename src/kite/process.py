28b67792eb570d28c39fc8fcba6d52d25cb20567
Dawid-Sroka <94313700+Dawid-Sroka@users.noreply.github.com>
2024-07-31 22:43:08 +0200

    Add separate OFOs for pipes ends

--- src/kite/process.py
+++ src/kite/process.py
@@ -62,6 +62,7 @@ class RegularFile(OpenFileObject):
         return
 
     def write(self, bytes_to_write):
+        position = 0
         f = self.file_struct
         f.seek(position)
         f.write("written\n")
@@ -76,59 +77,48 @@ class PipeBuffer():
         self.read_position = 0
         self.unread_count = 0
 
-
-class Pipe(OpenFileObject):
-    def __init__(self, file_name, file_struct, mode):
+class PipeReadEnd(OpenFileObject):
+    def __init__(self, file_name, file_struct):
         super().__init__(file_name, file_struct)
-        self.mode = mode
         self.ref_cnt = 1
         self.referenced_by = []
+        self.write_end_ptr = None
 
-        if mode == "rd":
-            def read(self, n_to_read):
-                array_of_returned_bytes = []
-                n_left = n_to_read
-                while n_left > 0:
-                    pipe_buffer = self.file_struct
-                    print("pipe buf: ", f)
-                    print(n_left)
+    def read(self, n_to_read):
+        array_of_returned_bytes = []
+        n_left = n_to_read
+        pipe_buffer = self.file_struct
+        while n_left > 0:
+            print("pipe buf: ", self.file_struct)
+            print(n_left)
 
-                    bytes_read = []
-                    chunk_len = min(n_left, self.unread_count)
-                    for _ in range(chunk_len):
-                        bytes_read.append(pipe_buffer.buffer[pipe_buffer.read_position])
-                        pipe_buffer.buffer[pipe_buffer.read_position] = None
-                        pipe_buffer.read_position = (pipe_buffer.read_position + 1) % pipe_buffer.buffer_size
-                        pipe_buffer.unread_count -= 1
-                    array_of_returned_bytes += bytes_read
+            bytes_read = []
+            chunk_len = min(n_left, pipe_buffer.unread_count)
+            for _ in range(chunk_len):
+                bytes_read.append(pipe_buffer.buffer[pipe_buffer.read_position])
+                pipe_buffer.buffer[pipe_buffer.read_position] = None
+                pipe_buffer.read_position = (pipe_buffer.read_position + 1) % pipe_buffer.buffer_size
+                pipe_buffer.unread_count -= 1
+            array_of_returned_bytes += bytes_read
 
-                    if bytes_read == []:
-                        print("     read blocked! What should happen now?")
-                        yield ("block", Resource("I/O",open_file_object))
-                    print(bytes_read)
-                    n_left -= chunk_len
-                return array_of_returned_bytes
+            if bytes_read == []:
+                print("     read blocked! What should happen now?")
+                yield ("block", Resource("I/O", self.write_end_ptr))
+            print(bytes_read)
+            n_left -= chunk_len
+        return array_of_returned_bytes
 
+class PipeWriteEnd(OpenFileObject):
+    def __init__(self, file_name, file_struct):
+        super().__init__(file_name, file_struct)
+        self.ref_cnt = 1
+        self.referenced_by = []
+        self.read_end_ptr = None
 
-# class Pipe:
-#     def __init__(self):
-#         self.buffer_size = 10
-#         self.buffer = [None] * self.buffer_size
-#         self.write_position = 0
-#         self.read_position = 0
-#         self.unread_count = 0
-
-#     def read(self, n_to_read):
-#         bytes_read = []
-#         for _ in range(min(n_to_read, self.unread_count)):
-#             bytes_read.append(self.buffer[self.read_position])
-#             self.buffer[self.read_position] = None
-#             self.read_position = (self.read_position + 1) % self.buffer_size
-#             self.unread_count -= 1
-#         return bytes_read
-
-#     def write(self, bytes_to_write):
-#         for i in range(min(self.buffer_size - self.unread_count, len(bytes_to_write))):
-#             self.buffer[self.write_position] = bytes_to_write[i]
-#             self.write_position = (self.write_position + 1) % self.buffer_size
-#             self.unread_count += 1
+    def write(self, bytes_to_write):
+        pipe_buffer = self.file_struct
+        for i in range(min(pipe_buffer.buffer_size - pipe_buffer.unread_count, len(bytes_to_write))):
+            pipe_buffer.buffer[pipe_buffer.write_position] = bytes_to_write[i]
+            pipe_buffer.write_position = (pipe_buffer.write_position + 1) % pipe_buffer.buffer_size
+            pipe_buffer.unread_count += 1
+        return ("unblock", Resource("I/O", self))
