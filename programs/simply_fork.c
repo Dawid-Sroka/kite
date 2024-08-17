2093aacd508704ab460139fdef36ebaeb61d96e5
Dawid-Sroka <94313700+Dawid-Sroka@users.noreply.github.com>
2024-08-17 19:26:35 +0200

    Update examples

--- programs/simply_fork.c
+++ programs/simply_fork.c
@@ -1,14 +1,16 @@
 #include <unistd.h>
 
 int main() {
+  int fd = open("file.log");
   int pid = fork();
+
   if (pid == 0) { // child
     execve("simply_write");
 
   } else {        // parent
-    int fd = open("file.log");
     char buf[10];
     read(fd, buf, 5);
+    write(2, buf, 10);
   }
   _exit();
 }
