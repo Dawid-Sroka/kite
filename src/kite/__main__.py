5c540ec07508d8cd7b03f308c62d2546801fead6
Dawid-Sroka <94313700+Dawid-Sroka@users.noreply.github.com>
2024-08-18 22:10:47 +0200

    Use typer for cli, path as argument

--- src/kite/__main__.py
+++ src/kite/__main__.py
@@ -1,22 +1,20 @@
 from kite.kernel import Kernel
 from pathlib import Path
 import logging
-from sys import argv
+import typer
 
-logging.basicConfig(
-    level=logging.INFO,  # Set the logging level
-    format='%(asctime)s - %(levelname)s - %(message)s',  # Log message format
-    handlers=[
-        logging.StreamHandler(),  # Log to stdout
-        logging.FileHandler('logfile.log', mode='w')  # Log to file
-    ]
-)
 
-kernel = Kernel.create()
+def main(path_to_binary: Path, debug: bool = False):
+    handlers = [logging.FileHandler('logfile.log', mode='w')]
+    if debug:
+        handlers.append(logging.StreamHandler())
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        handlers=handlers
+    )
 
-if len(argv) > 1:
-    program_name = argv[1]
-else:
-    program_name = "simply_exit"
+    kernel = Kernel.create()
+    kernel.start(path_to_binary)
 
-kernel.start(Path(__file__).parents[2] / "binaries" / program_name)
+typer.run(main)
