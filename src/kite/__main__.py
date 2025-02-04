bccf56a38d974bef3829e27b39f91e6ab4a41f99
Franciszek Pindel <franciszek.pin@gmail.com>
2025-02-04 15:36:07 +0100

    Remove logging number of cycles

--- src/kite/__main__.py
+++ src/kite/__main__.py
@@ -20,8 +20,7 @@ def main(path_to_binary: Path, debug: bool = False, simulator: str = "unicorn"):
         This filter injects cpu simulator's cycle count into the log.
         """
         def filter(self, record):
-            record.cycles_count = kernel.simulator.cpu.clock.cycles
-            pc = kernel.simulator.cpu.pc.r
+            pc = kernel.simulator.reg_read(REG_PC)
             record.pc = hex(pc) if pc > 0 else "before run"
             return True
 
@@ -30,7 +29,7 @@ def main(path_to_binary: Path, debug: bool = False, simulator: str = "unicorn"):
         handlers.append(logging.StreamHandler(stderr))
     logging.basicConfig(
         level=logging.INFO,
-        format='%(pc)s - %(cycles_count)s - %(message)s',
+        format='%(pc)s - %(message)s',
         handlers=handlers
     )
 
